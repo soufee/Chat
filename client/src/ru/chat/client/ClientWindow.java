281f85c8290908f5cddf1eac7cdef59c9eee8969
soufee <soufee@mail.ru>
2018-11-11 23:46:55 +0300

    Ready

--- client/src/ru/chat/client/ClientWindow.java
+++ client/src/ru/chat/client/ClientWindow.java
@@ -1,7 +1,94 @@
 package ru.chat.client;
 
-public class ClientWindow {
-    public static void main(String[] args) {
+import ru.network.TCPConnection;
+import ru.network.TCPConnectionListener;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
+    private static final String IP_ADDR = "127.0.0.1";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickName = new JTextField("noName");
+    private final JTextField fieldInput = new JTextField();
+    private TCPConnection connection;
+
+    private ClientWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        log.setEditable(false);
+        log.setLineWrap(true);
+        fieldInput.addActionListener(this);
+        add(log, BorderLayout.CENTER);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickName, BorderLayout.NORTH);
+        setVisible(true);
+        try {
+            connection = new TCPConnection(this, IP_ADDR, PORT);
+        } catch (IOException e) {
+            printMessage("Connection exception: " + e);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String msg = fieldInput.getText();
+         if (msg.equals("")) return;
+         else {
+             fieldInput.setText(null);
+             connection.sendString(fieldNickName.getText()+": "+msg);
+         }
+    }
+
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMessage("Connection ready...");
+
+    }
+
+    @Override
+    public void onRecieveString(TCPConnection tcpConnection, String value) {
+        printMessage(value);
+
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+        printMessage("Connection closed... ");
+
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMessage("Connection exception: " + e);
+    }
+
+    private synchronized void printMessage(String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + "\n");
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
     }
 }
--- server/src/ru/chat/server/ChatServer.java
+++ server/src/ru/chat/server/ChatServer.java
@@ -1,7 +1,61 @@
 package ru.chat.server;
 
-public class ChatServer {
-    public static void main(String[] args) {
+import ru.network.TCPConnectionListener;
+import ru.network.TCPConnection;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class ChatServer implements TCPConnectionListener {
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private ChatServer() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e) {
+                    System.out.println("TCPConnection exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected: "+tcpConnection);
+    }
+
+    @Override
+    public synchronized void onRecieveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnections(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client disconnected: "+tcpConnection);
+
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);
+    }
+
+    private void sendToAllConnections(String value) {
+        System.out.println(value);
+        final int cnt = connections.size();
+        for (int i = 0; i < cnt; i++)
+            connections.get(i).sendString(value);
     }
 }
